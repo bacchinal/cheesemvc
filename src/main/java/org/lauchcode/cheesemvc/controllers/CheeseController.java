a684748b576ca631bc64c57d9518f968b9e07895
Louis Coffman <bacchinal@gmail.com>
2019-08-25 17:01:25 -0500

    completed prep 3.8

--- src/main/java/org/lauchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/lauchcode/cheesemvc/controllers/CheeseController.java
@@ -1,7 +1,10 @@
 package org.lauchcode.cheesemvc.controllers;
 
+import org.lauchcode.cheesemvc.models.Cheese;
+import org.lauchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,29 +15,43 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
-    public String idex(Model model){
+        public String index(Model model) {
 
+            model.addAttribute("cheeses", CheeseData.getAll());
+            model.addAttribute("title", "My Cheeses");
 
-
-        model.addAttribute("cheeses", cheeses);
-        model.addAttribute("title", "My Cheeses");
-        return "cheese/index";
-
+            return "cheese/index";
     }
+
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseForm(Model model){
-        model.addAttribute("title", "Add Cheese");
-        return "cheese/add";
+        public String displayAddCheeseForm(Model model) {
+            model.addAttribute("title", "Add Cheese");
+            return "cheese/add";
     }
-    @RequestMapping( value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
 
-        return "redirect:";
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+        public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+            CheeseData.add(newCheese);
+            return "redirect:";
+        }
 
-    }
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+        public String displayRemoveCheeseForm(Model model) {
+            model.addAttribute("cheeses", CheeseData.getAll());
+            model.addAttribute("title", "Remove Cheese");
+            return "cheese/remove";
+        }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+        public String processRemoveCheeseForm(@RequestParam  int[] cheeseIds) {
+
+            for (int cheeseId : cheeseIds) {
+                CheeseData.remove(cheeseId);
+            }
+
+            return "redirect:";
+        }
 }
